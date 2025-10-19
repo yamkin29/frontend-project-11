94293e1790e0526620ad39e445142aaa077cf802
Aleksey Yamkin <me@jamkin.ru>
2025-10-19 16:05:46 +0300

    Add Vite in project

--- Makefile
+++ Makefile
@@ -1,9 +1,6 @@
 install:
 	npm ci
 
-publish:
-	npm publish --dry-run
-
 lint:
 	npx eslint .
 
--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,30 @@
 import pluginJs from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
+import globals from "globals";
 
 export default [
-  pluginJs.configs.recommended,
-  stylistic.configs.recommended,
   {
-    files: ['**/*.{js,ts,tsx}'],
     ignores: [
       'node_modules/**',
       'dist/**',
       'coverage/**',
       'eslint.config.js',
     ],
+  },
 
+  pluginJs.configs.recommended,
+  stylistic.configs.recommended,
+
+  {
+    files: ['src/**/*.{js,ts,tsx}'],
     languageOptions: {
       sourceType: 'module',
       ecmaVersion: 'latest',
-      globals: { console: 'readonly' },
+      globals: {
+        ...globals.browser,
+        console: 'readonly',
+      },
     },
-
     rules: {
       '@stylistic/indent': ['error', 2, { SwitchCase: 1 }],
     },
